52b4e212f5498f5583e06c6f5a5e5e9758fe8abe
shou.nakatani <the.gj.sky@gmail.com>
2021-12-15 08:30:51 +0000

    second

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,19 +1,19 @@
 def fizzbuzz(num)
 
 if num%3 == 0 && num%5 ==0
-    puts "FizzBuzz"
+    "FizzBuzz"
 elsif num%3 == 0
-    puts "fizz"
+    "fizz"
 elsif num%5 == 0
-    puts "Buzz"
+    "Buzz"
 else
-    puts num.to_s
+    num
 end
 end
 
 num_max = 100
 (1..num_max).each do |num|
     
-fizzbuzz = fizzbuzz(num)
+puts fizzbuzz = fizzbuzz(num)
 
 end
